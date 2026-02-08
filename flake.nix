74643dd6d7acccd586e79e4c92238bd2dcc90fab
adeci <alex.decious@gmail.com>
2026-02-07 21:39:25 -0500

    nixpkgs master track for ccb

--- flake.nix
+++ flake.nix
@@ -3,6 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs-master.url = "github:nixos/nixpkgs/master";
 
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
--- modules/adeci/dev.nix
+++ modules/adeci/dev.nix
@@ -1,14 +1,21 @@
 {
   pkgs,
+  inputs,
   ...
 }:
+let
+  pkgs-master = import inputs.nixpkgs-master {
+    system = pkgs.stdenv.hostPlatform.system;
+    config.allowUnfree = true;
+  };
+in
 {
 
   programs.direnv.enable = true;
 
   environment.systemPackages = with pkgs; [
     awscli2
-    claude-code-bin
+    pkgs-master.claude-code-bin # from master for latest updates
     gh
     jujutsu
     nixpkgs-review
