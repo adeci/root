45147b5aebd67ef76d55b2cf2f71ddaf67d293d8
adeci <alex.decious@gmail.com>
2026-03-27 17:31:07 -0400

    machines: delete NixOS home.nix files (superseded by wrapped packages)

--- machines/sequoia/home.nix
+++
@@ -1,9 +0,0 @@
-{ osConfig, ... }:
-{
-  imports = [
-    ../../modules/home-manager/profiles/base.nix
-    ../../modules/home-manager/profiles/llm-tools.nix
-  ];
-
-  home.stateVersion = osConfig.system.stateVersion;
-}
