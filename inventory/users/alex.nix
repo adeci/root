bb67097a6a04eca65948a69f08fc8c1aa5e42685
adeci <alex.decious@gmail.com>
2026-03-28 00:22:08 -0400

    tpmkey praxis add

--- inventory/users/alex.nix
+++ inventory/users/alex.nix
@@ -13,8 +13,8 @@
   ];
   sshKeys = [
     "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIB1VQn5GADumTi+/slg5hcEG2p2q28pAV83diGoowvZTAAAACXNzaDpzcGFyaw== spark"
-    "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIOf0URzmusd83C/zYSVyJotzH+hqL3KiKAVlxTvuDpYqAAAACXNzaDplbWJlcg== ember
-"
+    "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIOf0URzmusd83C/zYSVyJotzH+hqL3KiKAVlxTvuDpYqAAAACXNzaDplbWJlcg== ember"
+    "ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBODjmeH4hzi0wWwiJVoWBj2Kz5kKRz+hIaxn6nc9WrIFMp/coHtcvzFjukiCG7IbuR+aKczwyhYZrc33U4PTK2A= praxis-tpm"
 
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJVB44hBiASLPelTC//teEK3CpzrwswdBccLe9MKbaMp adecigear"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJeeoL1jwVSachA9GdJxm/5TgCRBULfSDGLyP/nfmkMq alex@DESKTOP-SVRV9Q8"
