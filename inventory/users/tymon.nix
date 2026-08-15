204ce41306e84d88d6eaef2ccfcf6a686b65aa36
adeci <alex.decious@gmail.com>
2026-08-14 22:52:51 -0400

    tymon added to leviathan

---
+++ inventory/users/tymon.nix
@@ -0,0 +1,15 @@
+{
+  description = "Tymon";
+  uid = 3803;
+  shell = "bash";
+  groups = [
+    "networkmanager"
+    "video"
+    "audio"
+    "input"
+    "kvm"
+  ];
+  sshKeys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIKzvi2CAfO35TVUewZV8XIKisF5IMJfFU0mI5KBoolBi Larsneef@Arch"
+  ];
+}
